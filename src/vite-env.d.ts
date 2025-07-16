33e8e6c5a7c0789d383342c5324ac03e012f8cc8
tozzo <tozzo@example.com>
2025-07-16 15:10:17 -0300

    Fix Vite types error and JSX namespace

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module '*.tsx' {
-  import React from 'react';
-  const component: React.FunctionComponent<any>;
-  export default component;
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
 } 
