8b853c701b0affb6fdcc81956639694e625b3af6
tozzo <tozzo@example.com>
2025-07-28 21:59:33 -0300

    Corrigir dependências do Vite para funcionar no Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: true
+    outDir: 'dist'
   }
 })
